d9a62f0a2c48afadd685dd55eb8aace4e5d9435e
abhash-er <abhashgenuineacc11@gmail.com>
2022-07-13 12:15:11 +0200

    latest changes

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import torch
 import os
 import clip
@@ -58,7 +59,7 @@ def convert_models_to_fp32(model):
 def train(model, preprocess, data_file, att_evaluator, device, output_folder, epoch_load=0, isModelSave=False):
     # Load the Training Data
     train_data = CustomDataset(data_file, preprocess, train=True)
-    train_dataloader = DataLoader(train_data, batch_size=10, shuffle=True)
+    train_dataloader = DataLoader(train_data, batch_size=16, shuffle=True)
 
     # Tensorboard logging
     timestamp = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
@@ -73,7 +74,7 @@ def train(model, preprocess, data_file, att_evaluator, device, output_folder, ep
 
     # Initialize optimizer and Optimizer
     loss = torch.nn.BCELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=5e-5, betas=(0.9, 0.98), eps=1e-6, weight_decay=0.2)
+    optimizer = torch.optim.Adam(model.visual.parameters(), lr=1e-4, betas=(0.9, 0.98), eps=1e-6, weight_decay=0.2)
 
     # Load Saved Model
     continue_training = False
@@ -102,29 +103,37 @@ def train(model, preprocess, data_file, att_evaluator, device, output_folder, ep
     for epoch in range(start_epoch, end_epoch):
         print("Epoch: ", epoch)
         for iteration, (images, labels) in enumerate(tqdm(train_dataloader, desc="Training Loop")):
-            optimizer.zero_grad()
+            print(images.shape)
+            a, b, c, d = images.shape
+            # plt.imshow(images.view(a, c, d, b)[0])
             images = images.to(device)
             labels = labels.to(device)
 
+            optimizer.zero_grad()
+
             image_features = model.encode_image(images).to(device)
-            norm = torch.norm(image_features, dim=-1, keepdim=True).detach()
-            image_features /= norm
+            # norm = torch.norm(image_features, dim=-1, keepdim=True).detach()
+            # image_features /= norm
 
             logits = image_features @ text_embedding.T
-            logits = logits.flatten().sigmoid().float()
-            labels = labels.flatten().float()
+            logits = logits.sigmoid().float()
+            labels = labels.float()
 
+            scores_overall, scores_per_class = att_evaluator.evaluate(pred=logits.detach().numpy().copy(),
+                                                                      gt_label=labels.detach().numpy().copy())
+            map_meter.add(scores_per_class['all']['ap'])
+
+            logits = logits.flatten()
+            labels = labels.flatten()
             mask = labels < 2
             if mask.sum() != 0:
                 logits = logits[mask]
                 labels = labels[mask]
+            print("Logits: ", logits)
+            print("Labels: ", labels)
 
-            if (labels == 1).sum() > 0:
-                ap = average_precision_score(labels.detach().numpy().copy(), logits.detach().numpy().copy())
-            else:
-                ap = 0
-            map_meter.add(ap * 100)
             total_loss = loss(logits, labels)
+            print("Loss: ", total_loss.item())
             # make_dot(total_loss).render("clip_torchviz", format="png")
             total_loss.backward()
 
@@ -136,7 +145,7 @@ def train(model, preprocess, data_file, att_evaluator, device, output_folder, ep
                 optimizer.step()
                 clip.model.convert_weights(model)
 
-            if iteration % 20 == 0:
+            if iteration % 40 == 0:
                 log.add_scalar("training_loss", train_loss_meter.mean, global_step)
                 log.add_scalar("Mean Average Precision", map_meter.mean, global_step)
                 train_loss_meter.reset()
@@ -161,26 +170,6 @@ def train(model, preprocess, data_file, att_evaluator, device, output_folder, ep
                     log.add_scalar("Validation Loss", validation_loss.item(), global_step)
 
             global_step += 1
-
-        # Validate on every 2 epochs
-        # if epoch % 2 == 0:
-        #     ground_truth, preds = validate(model, preprocess, data_file, device)
-        #     val_output_folder = check_dir(output_folder + "/epoch{}".format(epoch))
-        #     val_output_path = os.path.join(val_output_folder, "epoch{}".format(epoch))
-        #     att_evaluator.print_evaluation(
-        #         pred=preds.copy(),
-        #         gt_label=ground_truth.copy(),
-        #         output_file=val_output_path,
-        #     )
-        #     with torch.no_grad():
-        #         preds = preds.flatten()
-        #         ground_truth = ground_truth.flatten()
-        #         mask = ground_truth < 2
-        #         if mask.sum() != 0:
-        #             preds = preds[mask]
-        #             ground_truth = ground_truth[mask]
-        #         validation_loss = loss(torch.from_numpy(preds).float(), torch.from_numpy(ground_truth).float())
-        #         log.add_scalar("Validation Loss vs epochs", validation_loss.item(), epoch)
 
         # Save the model every 5 epochs
         if epoch % 5 == 0 and isModelSave:
@@ -255,7 +244,7 @@ if __name__ == "__main__":
 
     # Evaluate Predictions
     scores_overall, scores_per_class = evaluator.evaluate(pred=preds.copy(), gt_label=ground_truth.copy())
-
+    print(scores_per_class)
     results = evaluator.print_evaluation(
         pred=preds.copy(),
         gt_label=ground_truth.copy(),
