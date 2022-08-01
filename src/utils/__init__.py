2f09635f289bc993fac788fd6800c589eb5546b3
abhash-er <abhash@instance-dl-lab.us-central1-a.c.multi-label-att-text.internal>
2022-08-01 09:46:44 +0000

    raw caption training

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,14 @@
 import os
+import pickle
+import random
+from tqdm.auto import tqdm 
+
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
 from torch.utils.data import Dataset
 from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize, ColorJitter, RandomHorizontalFlip
+
 
 try:
     from torchvision.transforms import InterpolationMode
@@ -81,3 +86,85 @@ class CustomDataset(Dataset):
         # plt.imshow(image.permute((1, 2, 0)))
         # plt.show()
         return image, self.labels[idx]
+
+
+class CaptionDataset(Dataset):
+    def __init__(self, json_file, model, device, encode_text_fun, transform=None, train=True):
+        self.json_file = json_file
+        self.images_map = json_file["images"]
+        self.model = model
+        self.device = device
+        self.root = "datasets/coco/train2017/"
+        self.encode_text = encode_text_fun
+
+        if transform is not None:
+            self.transform = transform
+        else:
+            self.transform = Compose([
+                Resize((224, 224), interpolation=BICUBIC),
+                ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
+                # CenterCrop(224),
+                RandomHorizontalFlip(),
+                _convert_image_to_rgb,
+                ToTensor(),
+                Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+            ])
+        self.caption_embeddings = []
+        self.labels = []
+        check_dir("datasets/captions")
+        if not os.path.exists("datasets/captions/caption_embeddings.pkl"):
+            for idx in range(len(self.images_map)):
+                self.fill_items(idx)
+
+            with open("datasets/captions/caption_embeddings.pkl", 'wb') as t:
+                pickle.dump(self.caption_embeddings, t)
+
+            with open("datasets/captions/labels.pkl", 'wb') as t:
+                pickle.dump(self.labels, t)
+        else:
+            with open("datasets/captions/caption_embeddings.pkl", 'rb') as f:
+                self.caption_embeddings = pickle.load(f)
+
+            with open("datasets/captions/labels.pkl", 'rb') as f:
+                self.labels = pickle.load(f)
+
+    def fill_items(self, idx):
+        img_id = self.images_map[idx]['id']
+        caption_label_dict = {}
+        captions = []
+        negative_ann = []
+        for annotation in self.json_file['annotations']:
+            if annotation["image_id"] == img_id:
+                caption_label_dict[annotation["caption"]] = 1
+                captions.append(annotation["caption"])
+            else:
+                negative_ann.append(annotation)
+
+        # randomly sample 5 other negative captions
+        negative_ann = random.sample(negative_ann, 5)
+        for annotation in negative_ann:
+            caption_label_dict[annotation["caption"]] = 0
+            captions.append(annotation["caption"])
+
+        random.shuffle(captions)
+        label_caption = []
+        for cap in captions:
+            label_caption.append(caption_label_dict[cap])
+        self.labels.append(label_caption)
+        # print(len(self.labels))
+        # print(len(self.labels[0]))
+
+        token_caption, caption_embedding = self.encode_text(self.model, captions, device=self.device)
+        # print(caption_embedding.shape)
+        self.caption_embeddings.append(caption_embedding)
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        img_path = "datasets/coco/train2017/" + self.images_map[idx]['file_name']
+        img = Image.open(img_path)
+        img = self.transform(img)
+        assert len(self.labels[idx]) >= 10
+        # print(self.labels[idx])
+        return img, self.caption_embeddings[idx][:10], self.labels[idx][:10]
--- src/utils/models.py
+++ src/utils/models.py
@@ -59,3 +59,76 @@ class ResNet50Embedding(nn.Module):
         x = self.backbone(x)
         x = self.attribute_head(x)
         return x
+
+class ResNet50EmbeddingICM(nn.Module):
+    def __init__(self, pretrained, path_att_emb, num_captions=10):
+        super().__init__()
+        self.backbone = torchvision.models.resnet50(pretrained=pretrained)
+
+        # zs_weight = torch.tensor(np.load(path_att_emb), dtype=torch.float32)
+        zs_weight = torch.load(path_att_emb)
+        # already normalized
+        zs_weight = F.normalize(zs_weight, p=2, dim=1)
+        self.num_attributes, self.zs_weight_dim = zs_weight.shape
+
+        self.backbone.fc = nn.Linear(2048, self.zs_weight_dim)
+        nn.init.xavier_uniform_(self.backbone.fc.weight)
+
+        self.attribute_head = nn.Linear(self.zs_weight_dim, self.num_attributes)
+        self.attribute_head.weight.data = zs_weight.float()
+        self.attribute_head.bias.data = torch.zeros_like(self.attribute_head.bias.data).float()
+        self.attribute_head.weight.requires_grad = False
+        self.attribute_head.bias.requires_grad = False
+
+    def forward(self, x, y=None):
+        if y is not None:
+            x = self.backbone(x)
+            y.requires_grad = False
+            y = y.float()
+            x = x.unsqueeze(1).bmm(y.permute(0, 2, 1)).squeeze(1)
+            return x.T
+        else:
+            x = self.backbone(x)
+            x = self.attribute_head(x)
+            return x
+
+class ClipCaptionModel(nn.Module):
+    def __init__(self, backbone_type, path_att_emb, num_captions=10):
+        super().__init__()
+        # device=torch.device("cpu") is important to load weights in float32
+        backbone, _ = clip.load(backbone_type, jit=False, device=torch.device("cpu"))
+        self.backbone = backbone.visual
+
+        zs_weight = torch.load(path_att_emb)
+        zs_weight = F.normalize(zs_weight, p=2, dim=1)
+        self.num_attributes, self.zs_weight_dim = zs_weight.shape
+
+        if "RN" in backbone_type:
+            self.fc = nn.Linear(1024, self.zs_weight_dim)
+            self.fc_caption = nn.Linear(1024, self.zs_weight_dim)
+        else:
+            self.fc = nn.Linear(512, self.zs_weight_dim)
+            self.fc_caption = nn.Linear(512, self.zs_weight_dim)
+
+        nn.init.xavier_uniform_(self.fc.weight)
+        nn.init.xavier_uniform_(self.fc_caption.weight)
+
+        self.attribute_head = nn.Linear(self.zs_weight_dim, self.num_attributes)
+        self.attribute_head.weight.data = zs_weight.float()
+        self.attribute_head.bias.data = torch.zeros_like(self.attribute_head.bias.data).float()
+        self.attribute_head.weight.requires_grad = False
+        self.attribute_head.bias.requires_grad = False
+
+    def forward(self, x, y=None):
+        if y is not None:
+            x = self.backbone(x)
+            x = self.fc_caption(x)
+            y.requires_grad = False
+            y = y.float()
+            x = x.unsqueeze(1).bmm(y.permute(0, 2, 1)).squeeze(1)
+            return x.T
+        else:
+            x = self.backbone(x)
+            x = self.fc(x)
+            x = self.attribute_head(x)
+            return x
